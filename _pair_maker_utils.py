8e479ef8ce1edbf20333de99ed36e188b6134de4
morriscb <morrison.chrisb@gmail.com>
2016-02-17 15:43:03 +0100

    Changes to _pair_maker_utils
    _pair_maker_utils: Code no longer stores distance per unknown object but
    inverse distance at the pixel center per unknown object. Likewise the
    code also stores the inverse distance for the random objects as well.
    _pair_maker_utils: Docstring updates througout.

--- _pair_maker_utils.py
+++ _pair_maker_utils.py
@@ -17,6 +17,9 @@ class RawPairFinder(object):
     def __init__(self, unknown_itree, target_vector, target_ids,
                  stomp_map):
         """
+        Initialization for the pair finding software. Utilizes the STOMP
+        sphereical pixelization library to find and store all close pairs into
+        a HDF5 data file.
         args:
             unknown_itree: stomp IndexedTreeMap object containing the unknown
                 object sample. The data structure is that of a searchable
@@ -37,17 +40,17 @@ class RawPairFinder(object):
     def _reset_array_data(self):
         """
         Utility function that creates/resets the interal data storage of the 
-        class for the unknown sample
+        class for the unknown sample.
         """   
         self._area_array = np.empty(self._target_vect.size())
         self._unmasked_array = np.empty(self._target_vect.size())
         self._bin_resolution = np.empty(self._target_vect.size(),
                                         dtype = np.uint32)
         self._pair_list = []
-        self._pair_dist_list = []
+        self._pair_invdist_list = []
         for idx in xrange(self._target_vect.size()):
             self._pair_list.append([])
-            self._pair_dist_list.append([])
+            self._pair_invdist_list.append([])
             
         return None
     
@@ -55,23 +58,28 @@ class RawPairFinder(object):
         """
         Utility function that creates/resets the interal data storage of the 
         class for the random sample following the geometry of the unknown
-        sample.
-            
+        sample.  
         """ 
         self._n_random_per_target = (np.ones_like(self._target_ids,
                                                   dtype = np.uint32) * -99)
+        self._n_random_invdist_per_target = (
+            np.ones_like(self._target_ids, dtype = np.float32) * -99)
         
         return None
             
     def find_pairs(self, min_scale, max_scale):
         """
-        args:
+        Main functionality of the RawPairFinder class. Given the input data,
+        we find all the close pairs between the target, known redshift objects
+        and the unknown redshift oobjects. Stores the raw pair indices and also
+        the inverse distance weight.
+        Args:
             min_scale: float value of the minimum scale to run the pair finder.
                 Units are physical kpc
             max_scale: float value of the maximum scale to run the pair finder.
                 Units are physical kpc
-            n_randoms: int value factor of randoms to run. The total number
-                randoms used will be n_randoms * (# unknown points)
+        Returns:
+           None
         """
         
         self._reset_array_data()
@@ -110,18 +118,25 @@ class RawPairFinder(object):
                 
                 unmasked_frac += tmp_unmasked
                 area += tmp_unmasked * pix.Area(radial_bin.Resolution())
+                dist = (target_pix.Ang()).AngularDistance(pix.Ang())
                 
                 tmp_i_ang_vect = stomp.IAngularVector()
                 self._unknown_itree.Points(tmp_i_ang_vect, pix)
                 for i_ang in tmp_i_ang_vect:
                     self._pair_list[target_idx].append(i_ang.Index())
-                    self._pair_dist_list[target_idx].append(
-                        i_ang.AngularDistance(target_pix.Ang()))
+                    self._pair_invdist_list[target_idx].append(
+                        np.float32(1. / dist))
             self._area_array[target_idx] = area
             self._unmasked_array[target_idx] = unmasked_frac
             
+        return None
+            
     def random_loop(self, min_scale, max_scale, random_tree):
         """
+        Function for computing and storing the number of random objects, created
+        to follow the same geometry as the unknown sample, against of the target
+        sample. Stores the raw number counts and also an inverse weighted number
+        count.
         args:
             min_scale: float value of the minimum scale to run the pair finder.
                 Units are physical kpc
@@ -129,10 +144,11 @@ class RawPairFinder(object):
                 Units are physical kpc
             n_randoms: int value factor of randoms to run. The total number
                 randoms used will be n_randoms * (# unknown points)
+        Returns:
+            None
         """
         
         self._reset_random_data()
-        
         
         self._n_random_points = random_tree.NPoints()
         
@@ -158,6 +174,7 @@ class RawPairFinder(object):
                                      radial_bin, covering_pix_vect)
             
             n_points = 0
+            inv_dist = 0.0
             
             for pix in covering_pix_vect:
                 
@@ -165,9 +182,13 @@ class RawPairFinder(object):
                 if tmp_unmasked <= 0.0:
                     continue
                 
-                n_points += random_tree.NPoints(pix)
+                dist = (target_pix.Ang()).AngularDistance(pix.Ang())
+                tmp_n_points = random_tree.NPoints(pix)
+                n_points += tmp_n_points
+                inv_dist += tmp_n_points / dist
             
             self._n_random_per_target[target_idx] = n_points
+            self._n_random_invdist_per_target[target_idx] = np.float32(inv_dist)
             
         return None
     
@@ -200,6 +221,10 @@ class RawPairFinder(object):
                 '%i' % self._target_ids[target_idx],
                 data = np.array(self._pair_list[target_idx],
                                 dtype = np.uint32))
+            tmp_grp.create_dataset(
+                '%i_invdist' % self._target_ids[target_idx],
+                data = np.array(self._pair_invdist_list[target_idx],
+                                dtype = np.float32))
             tmp_data_set.attrs.create('redshift',
                                       target.Redshift())
             tmp_data_set.attrs.create('unmasked_frac',
@@ -211,6 +236,9 @@ class RawPairFinder(object):
             try:
                 tmp_data_set.attrs.create(
                     'rand', self._n_random_per_target[target_idx])
+                tmp_data_set.attrs.create(
+                    'rand_invdist',
+                    self._n_random_invdist_per_target[target_idx])
             except AttributeError:
                 continue
             
