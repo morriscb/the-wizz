Added region density weighting and fully added fixed bootstrap samples.
stomp_mask_catalog.py: Added regionation and storing of the region id to
the output fits file.
_pair_maker_utils.py: Added storing of region area and total area to the
output HDF5 file.
_pdf_maker_utils.py: Added calculation of region densities to the code.
This reweights the randoms based on the over-density in the stomp map
region compared to the global over-density.
input_flags.py: Added unknown_stomp_region_name and
output_bootstraps_file to flags.
pdf_maker: Added writing of the individual bootstrap subsamples.

_pdf_maker_utils.py
@@ -91,6 +91,16 @@ def collapse_ids_to_single_estimate(hdf5_pairs_group, unknown_data, args):
     
     rand_ratio = (unknown_data.shape[0] /
                   (1. * hdf5_pairs_group.attrs['n_random_points']))
+    if args.unknown_stomp_region_name is not None:
+        tmp_n_region = np.array(
+            [unknown_data[unknown_data[args.unknown_stomp_region_name] ==
+                          reg_idx].shape[0]
+             for reg_idx in xrange(hdf5_pairs_group.attrs['n_region'])],
+                                dtype = np.int_)
+        rand_ratio = (
+            (tmp_n_region / (1. * hdf5_pairs_group.attrs['n_random_points'])) *
+            (hdf5_pairs_group.attrs['area'] /
+             hdf5_pairs_group.attrs['region_area']))
     id_array = unknown_data[args.unknown_index_name]
     id_args_array = id_array.argsort()
     id_array = id_array[id_args_array]
@@ -252,7 +262,15 @@ class PDFMaker(object):
             None
         """
         
-        self.target_rand_array *= rand_ratio * ave_weight
+        try:
+            tmp_rand_ratio = rand_ratio[self.target_region_array]
+        except TypeError:
+            tmp_rand_ratio = rand_ratio
+        try:
+            tmp_ave_weight = ave_weight[self.target_region_array]
+        except TypeError:
+            tmp_ave_weight = ave_weight
+        self.target_rand_array *= tmp_rand_ratio * tmp_ave_weight
         
         return None
     
@@ -457,7 +475,7 @@ class PDFMaker(object):
         
         return None
     
-    def write_bootstrap_samples_to_ascii(self, output_name):
+    def write_bootstrap_samples_to_ascii(self, output_name, args):
         
         """
         Method for writing the individual bootstrap samples to ascii.
@@ -465,11 +483,18 @@ class PDFMaker(object):
             output_name: string specifying the name of the ascii file to write
                 the pdf/density results to. By default any existing file will
                 be overwritten.
+            args: ArgumentParser.parse_args object returned from
+                input_flags.parse_input_pdf_args
         Returns:
             None
         """
         
-        np.savetxt(output_name, self.bootstrap_array)
+        output_header = '# input_flags:\n'
+        for arg in vars(args):
+            output_header += '#\t%s : %s\n' % (arg, getattr(args, arg))
+        
+        np.savetxt(output_name, self.bootstrap_array, fmt = '%.8f',
+                   header = output_header)
         
         return None
     
@@ -501,7 +526,7 @@ class PDFMaker(object):
         for bin_idx in xrange(self.redshift_array.shape[0]):
             
             output_file.writelines(
-                '%.6e %.6e %.6e %.6e %.6e %.6e %.6e\n' %
+                '%.8e %.8e %.8e %.8e %.8e %.8e %.8e\n' %
                 (self.redshift_array[bin_idx], self.density_array[bin_idx],
                  self.density_err_array[bin_idx], self.unknown_array[bin_idx],
                  self.rand_array[bin_idx], self.area_array[bin_idx],

utility_programs/stomp_mask_catalog.py
@@ -14,14 +14,22 @@ random-random normalization term to line up with the data file used.
 if __name__ == "__main__":
     
     parser = argparse.ArgumentParser()
-    parser.add_argument('--stomp_map', default = '',
+    parser.add_argument('--stomp_map', required = True,
                         type = str, help = 'Name of the STOMP map defining the '
                         'geometry on the sky for the target and unknown '
                         'samples.')
-    parser.add_argument('--input_fits_file', default = '',
+    parser.add_argument('--n_regions', default = None,
+                        type = int, help = 'Number of sub resgions to break up '
+                        'the stomp map into for bootstrap/jackknifing. It is '
+                        'recommended that the region size be no smaller than '
+                        'the max scale requested in degrees.')
+    parser.add_argument('--region_column_name', default = 'stomp_region',
+                        type = str, help = 'Name of the column to write the '
+                        'region_id to.')
+    parser.add_argument('--input_fits_file', required = True,
                         type = str, help = 'Name of the fits catalog to load '
                         'and test against the stomp map geometry.')
-    parser.add_argument('--output_fits_file', default = '',
+    parser.add_argument('--output_fits_file', required = True,
                         type = str, help = 'Name of the fits file to write the '
                         'resultant masked data to.')
     parser.add_argument('--ra_name', default = 'ALPHA_J2000',
@@ -38,21 +46,33 @@ if __name__ == "__main__":
     
     ### Load the stomp map
     stomp_map = stomp.Map(args.stomp_map)
+    if args.n_regions is not None:
+        stomp_map.InitializeRegions(args.n_regions)
+        print("%i regions initialzied at %i resolution" %
+              (stomp_map.NRegion(), stomp_map.RegionResolution()))
     
     ### Create a empty mask of the objects considered from the input_fits_file
     mask = np.zeros(data.shape[0], dtype = np.bool)
+    if args.n_regions is not None:
+        region_array = np.empty(data.shape[0], dtype = np.uint32)
     for idx, obj in enumerate(data):
         tmp_ang = stomp.AngularCoordinate(obj[args.ra_name], obj[args.dec_name],
                                           stomp.AngularCoordinate.Equatorial)
         ### Test the current catalog object and see if it is contained in the
         ### stomp map geometry. Store the result.
         mask[idx] = stomp_map.Contains(tmp_ang)
+        if args.n_regions is not None and mask[idx]:
+            region_array[idx] = stomp_map.FindRegion(tmp_ang)
     
     ### Write file to disk and close the currently open fits file.
-    out_tbhdu = fits.BinTableHDU.from_columns(
-        [fits.Column(name = data.names[idx], format = data.formats[idx],
-                     array = data[data.names[idx]][mask])
-         for idx in xrange(len(data.names))])
-    out_tbhdu.writeto(args.output_fits_file)
+    col_list = [fits.Column(name = data.names[idx], format = data.formats[idx],
+                            array = data[data.names[idx]][mask])
+                for idx in xrange(len(data.names))]
+    if args.n_regions is not None:
+        col_list.append(fits.Column(name = args.region_column_name,
+                                    format = 'I',
+                                    array = region_array[mask]))
+    out_tbhdu = fits.BinTableHDU.from_columns(col_list)
+    out_tbhdu.writeto(args.output_fits_file, clobber = True)
     hdu.close()
     ### Done!