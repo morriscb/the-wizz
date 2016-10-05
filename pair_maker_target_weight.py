f43e718faa534fdb238221d927c4dd0b4f7d7eeb
Chris Morrison <morriscb@uw.edu>
2016-10-06 00:40:09 +0200

    Finalized pair_maker_target_weight.py conversion to LSST standards.

--- pair_maker_target_weight.py
+++ pair_maker_target_weight.py
@@ -1,16 +1,5 @@
 
-import _core_utils
-import _stomp_utils
-import input_flags
-import _pair_maker_utils
-import h5py
-import numpy as np
-import stomp
-import sys
-
-
-"""
-This program is used to calculate a relative weight for each spectroscpic
+"""This program is used to calculate a relative weight for each spectroscpic
 object. Dense structures in the target dataset can cause undue bias in the
 resultant recovery and even cause negagive correlations for samples that anti-
 correlation with said structures. This code attempts to mitigate this by
@@ -19,38 +8,37 @@ using this as a weight for supressing the effect of over-dense structures on
 the recovery. This may even help with surveys with strange masks.
 """
 
+from __future__ import division, print_function, absolute_import
+
+import numpy as np
+import stomp
+import sys
+
+import _core_utils
+import _pair_maker_utils
+import _stomp_utils
+import input_flags
+
 
 if __name__ == "__main__":
-    
     print("")
     print("The-wiZZ has begun conjuring: running pair maker target weight...")
-    
-    ### load the command line arguments 
+    # Load the command line arguments.
     args = input_flags.parse_input_pair_args()
-    
     input_flags.print_args(args)
-    
-    ### load the stomp geometry coving the area of spectroscopic overlap
-    ### TODO:
-    ###     create an external program that allows for the creation of an
-    ###     addaptive map. This will require some testing and visualization
-    ###     software for Python. Low priority, maybe at ver1.0. Easy fix would
-    ###     be a test of min, max RA, DEC and testing the area of that and how
-    ###     much area remains after that. User can set a threshold based on how
-    ###     much area coverage they know there should be.
+    # Load the stomp geometry coving the area of spectroscopic overlap.
     stomp_map = stomp.Map(args.stomp_map)
-    ### We request regionation for use with spatial bootstrapping. The
-    ### resolution found for regionation also sets the 
+    # We request regionation for use with spatial bootstrapping. The
+    # resolution found for regionation also sets the max resolution of the
+    # different STOMP tree maps.
     stomp_map.InitializeRegions(args.n_regions)
     print("Created %i Regions at resolution %i..." %
           (stomp_map.NRegion(), stomp_map.RegionResolution()))
-    
-    ### load the sample with known redshifts 
+    # Load the sample with known redshifts.
     target_vector, target_ids = _stomp_utils.load_target_sample(
         args.target_sample_file, stomp_map, args)
-    
-    ### Instead of loading an unknown sample from disk, we load the target
-    ### objects themselves as the unknown sample.
+    # Instead of loading an unknown sample from disk, we load the target
+    # objects themselves as the unknown sample.
     unknown_itree = stomp.IndexedTreeMap(stomp_map.RegionResolution(), 200)
     for target_idx, target_obj in enumerate(target_vector):
         tmp_i_ang = stomp.IndexedAngularCoordinate(
@@ -58,65 +46,52 @@ if __name__ == "__main__":
             target_obj.UnitSphereZ(), target_idx)
         unknown_itree.AddPoint(tmp_i_ang)
     print("%i Galaxies in tree" % unknown_itree.NPoints())
-    
-    ### We also wish to subtract a random sample from density estimate. This
-    ### function creates a set of uniform data points on the geometry of the
-    ### stomp map.
+    # We also wish to subtract a random sample from density estimate. This
+    # function creates a set of uniform data points on the geometry of the
+    # stomp map.
     if args.n_randoms > 0:
         random_tree = _stomp_utils.create_random_data(
             args.n_randoms * unknown_itree.NPoints(), stomp_map)
-    
-    ### now that we have everything set up we can send our data off to the pair
-    ### finder
+    # now that we have everything set up we can send our data off to the pair
+    # finder
     pair_finder = _pair_maker_utils.RawPairFinder(unknown_itree, target_vector,
                                                   target_ids, stomp_map)
-    
-    ### We need to tell the pair finder what scale we would like to run over
-    ### before we begin. 
+    # We need to tell the pair finder what scale we would like to run over
+    # before we begin.
     min_scale_list = args.min_scale.split(',')
     max_scale_list = args.max_scale.split(',')
-    
-    print min_scale_list
-    print max_scale_list
-    
+    print(min_scale_list)
+    print(max_scale_list)
     if len(min_scale_list) != len(max_scale_list):
         print("Number of min scales requested does not match number of max"
               "sales. Exitting.")
         sys.exit()
-        
     over_density_list = []
-    ### loop over the min/max scales requested.
+    # Loop over the min/max scales requested.
     output_file = _core_utils.create_ascii_file(args.output_pair_hdf5_file,
                                                 args)
     output_file.writelines('#type0 = redshift\n')
-    for scale_idx in xrange(len(min_scale_list)):
+    for scale_idx, min_scale, max_scale in enumerate(zip(min_scale_list,
+                                                         max_scale_list)):
         output_file.writelines('#type%i = kpc%st%s\n' %
-                               (scale_idx + 1,
-                                min_scale_list[scale_idx],
-                                max_scale_list[scale_idx]))
-        print("Running scale: %s to %s" % (min_scale_list[scale_idx],
-                                           max_scale_list[scale_idx]))
-        ### Pair finder does what it says. It also computes the areas, unmasked
-        ### fractions for target object.
-        pair_finder.find_pairs(np.float_(min_scale_list[scale_idx]),
-                               np.float_(max_scale_list[scale_idx]))
-        
-        ### This is an optional part of the pair finder. It takes as an argument
-        ### the a stomp tree map containing uniform random points as generated
-        ### by the stomp. For non-uniform randoms it is recommened that the
-        ### user run this software with said randoms as the unknown sample.
-        if args.n_randoms > 0: 
-            pair_finder.random_loop(np.float_(min_scale_list[scale_idx]),
-                                    np.float_(max_scale_list[scale_idx]),
+                               (scale_idx + 1, min_scale, max_scale))
+        print("Running scale: %s to %s" % (min_scale, max_scale))
+        # Pair finder does what it says. It also computes the areas, unmasked
+        # fractions for target object.
+        pair_finder.find_pairs(np.float_(min_scale), np.float_(max_scale))
+        # This is an optional part of the pair finder. It takes as an argument
+        # the a stomp tree map containing uniform random points as generated
+        # by the stomp. For non-uniform randoms it is recommened that the
+        # user run this software with said randoms as the unknown sample.
+        if args.n_randoms > 0:
+            pair_finder.random_loop(np.float_(min_scale), np.float_(max_scale),
                                     random_tree)
-            
         over_density_array = np.empty(len(pair_finder._pair_list))
         for target_idx, target_id_list in enumerate(pair_finder._pair_list):
             over_density_array[target_idx] = (
-                1. * len(target_id_list) * args.n_randoms /
-                (1. * pair_finder._n_random_per_target[target_idx]))
+                len(target_id_list) * args.n_randoms /
+                pair_finder._n_random_per_target[target_idx])
         over_density_list.append(over_density_array)
-    
     for target_idx, target_obj in enumerate(target_vector):
         line = '%.8e ' % target_obj.Redshift()
         for scale_idx in xrange(len(over_density_list)):
@@ -125,5 +100,4 @@ if __name__ == "__main__":
                 over_density_list[scale_idx][target_idx], 1.0)
         output_file.writelines(line + '\n')
     output_file.close()
-    ### And that's it. We are done.
-    
+    # And that's it. We are done.
