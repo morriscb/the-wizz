4ea3f831477fb9a257e8e429f3fee65c587f8ba6
Christopher Morrrison <morriscb@uw.edu>
2019-08-02 15:22:46 -0700

    Add first pass pair_collapser class/method.

--- the_wizz/pair_collapser.py
+++ the_wizz/pair_collapser.py
@@ -1,4 +1,5 @@
 
+from glob import glob
 from multiprocessing import Pool
 import numpy as np
 import pandas as pd
@@ -6,43 +7,127 @@ import pandas as pd
 from .pair_maker import decompress, distance_weight
 
 
+class PairCollapser:
+    """
+    """
+
+    def __init__(self,
+                 parquet_pairs,
+                 r_mins,
+                 r_maxes,
+                 z_min=0.01,
+                 z_max=5.00,
+                 weight_power=-0.8,
+                 n_proc=0):
+        self.parquet_pairs = parquet_pairs
+        self.r_mins = r_mins
+        self.r_maxes = r_maxes
+        self.z_min = z_min
+        self.z_max = z_max
+        self.weight_power = weight_power
+        self.n_proc = n_proc
+
+    def run(self,
+            unknown_catalog):
+        """
+        """
+        unkn_ids = unknown_catalog["id"]
+        try:
+            unkn_regions = unknown_catalog["region"]
+        except KeyError:
+            unkn_regions = np.zeros(len(unkn_ids), dtype=np.uint32)
+        try:
+            unkn_weights = unknown_catalog["weights"]
+        except KeyError:
+            unkn_weights = unknown_catalog["weights"]
+
+        unique_regions = np.unique(unkn_regions)
+
+        output = []
+        for region in unique_regions:
+            z_bin_paths = glob("%s/region=%i/z_bin=*" %
+                               (self.parquet_pairs, region))
+            region_mask = unkn_regions == region
+            region_ids = unkn_ids[region_mask]
+            region_weights = unkn_weights[region_mask]
+
+            process_data = [
+                {"unkn_ids": region_ids,
+                 "unkn_weights": region_weights,
+                 "r_mins": self.r_mins,
+                 "r_maxes": self.r_maxes,
+                 "file_name": self.parquet_pairs,
+                 "region": "region=%i" % region,
+                 "z_bin": z_bin}
+                for z_bin in z_bin_paths]
+            if self.n_proc > 0:
+                with Pool(self.n_proc) as pool:
+                    region_output = pool.imap_unordered(collapse_pairs,
+                                                        process_data)
+                output.extend(region_output)
+            else:
+                for data in process_data:
+                    output.append(collapse_pairs(data))
+
+        return pd.concat(output)
+
+
 def collapse_pairs(data):
     """
     """
-    output = {"id": ,
-              "redshift": ,
-              "region": ,}
+    output = []
     r_mins = data["r_mins"]
     r_maxes = data["r_maxes"]
-    pair_data = pd.read_parquet("%s/region=%i/ref_id=%i" % 
-                                (data["file_name"],
-                                 data["region"],
-                                 data["ref_id"]))
+    reference_data = pd.read_parquet("%s/%s/reference_data.parquet" %
+                                     (data["file_name"],
+                                      data["region"]))
+    reference_data.set_index("ref_id", inplace=True)
+
+    pair_data = pd.read_parquet("%s" % data["z_bin"])
+    pair_data.set_index("ref_id", inplace=True)
     unkn_ids = data["unkn_ids"]
     unkn_weights = data["unkn_weights"]
 
+    for ref_id in pair_data.index.unique():
+        output_row = collapse_pairs_ref_id(reference_data.loc[ref_id],
+                                           pair_data.loc[ref_id],
+                                           unkn_ids,
+                                           unkn_weights,
+                                           r_mins,
+                                           r_maxes)
+        output.append(output_row)
+
+    return pd.DataFrame(output)
+
+
+def collapse_pairs_ref_id(ref_row,
+                          pair_data,
+                          unkn_ids,
+                          unkn_weights,
+                          r_mins,
+                          r_maxes):
+    """
+    """
+    output = dict()
+    output["ref_id"] = ref_row["ref_id"]
+    output["redshift"] = ref_row["redshift"]
+    output["region"] = ref_row["region"]
+    for r_min, r_max in zip(r_mins, r_maxes):
+        scale_name = "Mpc%.2ft%.2f" % (r_min, r_max)
+        output["%s_counts" % scale_name] = 0
+        output["%s_weights" % scale_name] = 0.0
+
     pair_dists = decompress(pair_data["comp_log_dists"])
-    pair_dist_mask = np.logical_and(pair_dists >= np.min(r_mins),
-                                    pair_dists < np.max(r_maxes))
-    pair_ids = pair_data["ids"][pair_dist_mask]
-    pair_dists = pair_dists[pair_dist_mask]
+    pair_ids = pair_data["unkn_ids"]
 
     (start_idx, end_idx) = find_trim_indexes(pair_ids, unkn_ids)
     if start_idx == end_idx:
-        for r_min, r_max in zip(r_mins, r_maxes):
-            scale_name = "Mpc%.2ft%.2f" % (r_min, r_max)
-            output["%s_counts" % scale_name] = 0
-            output["%s_weights" % scale_name] = 0.0
         return output
     sub_unkn_ids = unkn_ids[start_idx: end_idx]
     sub_unkn_weights = unkn_weights[start_idx: end_idx]
 
     (start_idx, end_idx) = find_trim_indexes(sub_unkn_ids, pair_ids)
     if start_idx == end_idx:
-        for r_min, r_max in zip(r_mins, r_maxes):
-            scale_name = "Mpc%.2ft%.2f" % (r_min, r_max)
-            output["%s_counts" % scale_name] = 0
-            output["%s_weights" % scale_name] = 0.0
         return output
     sub_pair_ids = pair_ids[start_idx: end_idx]
     sub_pair_dists = pair_dists[start_idx: end_idx]
@@ -53,13 +138,17 @@ def collapse_pairs(data):
     else:
         matched_weights, matched_dists = find_pairs(
             sub_unkn_ids, sub_pair_ids, sub_unkn_weights, sub_pair_dists)
+
+    matched_dist_weights = matched_weights * distance_weight(matched_dists)
     for r_min, r_max in zip(r_mins, r_maxes):
         scale_name = "Mpc%.2ft%.2f" % (r_min, r_max)
         dist_mask = np.logical_and(matched_dists >= r_min,
                                    matched_dists < r_max)
         output["%s_counts" % scale_name] = dist_mask.sum()
-        output["%s_weights" % scale_name] = 
+        output["%s_weights" % scale_name] = \
+            matched_dist_weights[dist_mask].sum()
     return output
+
 
 def find_trim_indexes(trim_to, trim_from):
     """
